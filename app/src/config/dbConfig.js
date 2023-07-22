0c16a6c39b15d9d4d6e193528926260b87f61769
Dhruv9544 <dhruvshah905@gmail.com>
2023-07-22 22:50:44 +0530

    config

--- app/src/config/dbConfig.js
+++ app/src/config/dbConfig.js
@@ -4,5 +4,5 @@ require('dotenv').config()
 const {MONGO_URI} = process.env 
 
 module.exports.getDbConnection = function(){ 
-    mongoose.connect(MONGO_URI).then(()=>console.log("DbConnected")).catch(()=>console.log("DbConnection Fail"))
+    mongoose.connect(MONGO_URI).then(()=>console.log("DbConnected"))
 }
